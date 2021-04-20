c3bc5ab27e77ec90798e1ae59abcb64bdbe166dc
Olga Pelipets <pelipets.olga.developer@gmail.com>
2021-04-20 18:57:27 +0300

    Home screen

--- lib/data/api/api.dart
+++ lib/data/api/api.dart
@@ -13,9 +13,13 @@ abstract class BooksApi {
     "x-rapidapi-key": "326713088emsh32774d1e886ae24p1d9715jsncbeea767b6db",
     "x-rapidapi-host": "google-books.p.rapidapi.com"
   };
+  static const String _startIndex = "startIndex";
+  static const String _maxResults = "maxResults";
 
   factory BooksApi(Dio dio) => _BooksApi(dio);
 
   @GET("/volumes?key=AIzaSyB6evM_BbeqnZ8GIqHTDGwcgxXCCBJ8zlE")
-  Future<Volumes> getAllVolumes(@Query(_key) String apiKey);
+  Future<Volumes> getAllVolumes(@Query(_key) String apiKey,
+      {@Query(_startIndex) int startIndex = 0,
+      @Query(_maxResults) int maxResults = 10});
 }
--- lib/ui/home/home_widget.dart
+++ lib/ui/home/home_widget.dart
@@ -1,5 +1,5 @@
-import 'dart:developer';
-
+import 'package:books_app/domain/models/items.dart';
+import 'package:books_app/domain/models/volumes.dart';
 import 'package:books_app/domain/repository/repository.dart';
 import 'package:books_app/tools/logger.dart';
 import 'package:flutter/material.dart';
@@ -13,20 +13,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final repository = getIt.get<BooksRepository>();
-    final logger = getIt.get<Log>();
-
-    repository.getAllBooks().then((it) {
-      logger.log(logTag, it.toString());
-      it.items.forEach((element) {
-        logger.log(logTag, element.toString());
-      });
-    });
 
     return Scaffold(
       appBar: AppBar(title: Text("Books")),
-      body: Center(
-        child: Text("Home content"),
+      body: FutureBuilder(
+        future: repository.getAllBooks(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.data != null &&
+              snapshot.data is VolumesModel &&
+              (snapshot.data as VolumesModel).items.length > 0) {
+            return _buildListView(snapshot.data as VolumesModel);
+          }
+          if (snapshot.data != null) {
+            return Center(child: Text("There are some other data in snapshot"));
+          } else
+            return Center(child: CircularProgressIndicator());
+        },
       ),
     );
+  }
+
+  ListView _buildListView(VolumesModel volumesModel) {
+    return ListView.builder(
+        itemCount: volumesModel.items.length,
+        itemBuilder: (context, index) {
+          final thumbnailUrl =
+              volumesModel.items[index].volumeInfo.imageLinks?.thumbnail;
+          return Card(
+            child: ListTile(
+              title: Text(volumesModel.items[index].volumeInfo.title),
+              subtitle: Text(_getAuthors(volumesModel.items[index])),
+              leading: thumbnailUrl != null
+                  ? new Image.network(thumbnailUrl)
+                  : Icon(Icons.book),
+            ),
+          );
+        });
+  }
+
+  String _getAuthors(ItemsModel item) {
+    final authors = item.volumeInfo.authors;
+    if (authors == null || authors.isEmpty) return "";
+    return authors.join(", ");
   }
 }
