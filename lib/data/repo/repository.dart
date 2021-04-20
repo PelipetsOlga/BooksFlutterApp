6c8fd3fd4e900689eb837ed440b87f7e66897ee8
Olga Pelipets <pelipets.olga.developer@gmail.com>
2021-04-20 18:36:06 +0300

    Dependency Injection

--- lib/data/repo/repository.dart
+++ lib/data/repo/repository.dart
@@ -1,11 +1,18 @@
 import 'package:books_app/data/api/api.dart';
 import 'package:books_app/domain/models/volumes.dart';
 import 'package:books_app/domain/repository/repository.dart';
+import 'package:injectable/injectable.dart';
+import 'package:dio/dio.dart';
 
+@Singleton(as: BooksRepository)
 class BooksRepositoryImpl implements BooksRepository {
-  BooksApi api;
+  late BooksApi api;
 
-  BooksRepositoryImpl(this.api);
+  BooksRepositoryImpl() {
+    final dio = Dio();
+    dio.options.headers = BooksApi.headers;
+    this.api = BooksApi(dio);
+  }
 
   @override
   Future<VolumesModel> getAllBooks() async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
 import 'dart:developer';
 
-import 'package:books_app/data/api/api.dart';
 import 'package:flutter/material.dart';
 
-import 'data/repo/repository.dart';
+import 'domain/repository/repository.dart';
+import 'injection.dart';
 import 'navigation/navigation.dart';
 import 'ui/common/constants.dart';
-import 'package:dio/dio.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // configureDependencies();
+  configureDependencies();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final dio = Dio();
-    dio.options.headers = BooksApi.headers;
-    final api = BooksApi(dio);
-
-    final repository = BooksRepositoryImpl(api);
+    final repository = getIt.get<BooksRepository>();
 
     repository.getAllBooks().then((it) {
       log(it.toString());
+      it.items.forEach((element) {
+        log(element.toString());
+      });
     });
 
     return MaterialApp(
